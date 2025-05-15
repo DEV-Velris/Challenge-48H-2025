1296a56f7b68cb41b037fb075ae5389a6542885e
Velris <velris.dev@gmail.com>
2025-05-15 10:39:36 +0200

    Initialisation projet Web

---
+++ website/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Challenge 48H",
+  description: "Challenge 48H",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ website/src/ressources/icons/index.ts
@@ -0,0 +1,1 @@
+// Içi seront les .tsx utilisant les <Image> Optimisé de Next
