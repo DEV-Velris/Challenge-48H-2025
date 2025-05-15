44f9aa4ef72fe0863cbfe8e98eab832f05d702ab
Alexis <86017918+DEV-Velris@users.noreply.github.com>
2025-05-15 15:25:02 +0200

    Feature/cha 15 (#4)
    * Liaison websocket & website faite
    
    * Ajout .env pour websocket
    
    * Déplacement et ajout exemple .env
    
    * Amélioration .gitignore website
    
    ---------
    
    Co-authored-by: Velris <velris.dev@gmail.com>

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,18 +1,35 @@
 import {Server} from "socket.io";
 import {MessagePayload} from "@/_types/MessagePayload";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const websocket = new Server({
     cors: {
-        origin: "*"
+        origin: "*",
     }
 });
 
 const rooms = ["district1", "district2", "district3", "district4", "district5", "district6", "district7", "district8", "district9"];
 
+const messageHistory: Record<string, string[]> = {};
+
+rooms.forEach(room => {
+    messageHistory[room] = [];
+});
+
+const addMessageToHistory = (payload: MessagePayload) => {
+    messageHistory[payload.districtId].push(payload.message);
+    if (messageHistory[payload.districtId].length > 10) {
+        messageHistory[payload.districtId].shift();
+    }
+}
+
 websocket.on("connection", (socket) => {
     socket.on("joinChat", (districtId: string) => {
         if (rooms.includes(districtId)) {
             socket.join(districtId);
+            socket.emit("messageHistory", messageHistory[districtId]);
         } else {
             socket.emit("error", {message: "Invalid district ID"});
         }
@@ -21,9 +38,12 @@ websocket.on("connection", (socket) => {
     socket.on("message", (payload: MessagePayload) => {
         const {districtId, message} = payload;
         if (rooms.includes(districtId)) {
-            websocket.to(districtId).emit("message", message);
+            addMessageToHistory(payload);
+            websocket.in(districtId).emit("message", message);
         }
     });
 });
 
-websocket.listen(3000);
+websocket.listen(process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3001);
+
+console.log(`WebSocket server is running on port ${process.env.PORT !== undefined ? process.env.PORT : 3001}`);
