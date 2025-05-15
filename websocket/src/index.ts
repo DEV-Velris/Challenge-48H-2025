807010df449688b7761be74ce6028b8c064b2fb3
Velris <velris.dev@gmail.com>
2025-05-15 13:05:20 +0200

    Mise en place WebSocket avec exemple test de client

---
+++ websocket/src/index.ts
@@ -0,0 +1,27 @@
+import {Server} from "socket.io";
+
+const websocket = new Server({
+    cors: {
+        origin: "*"
+    }
+});
+
+const rooms = ["district1", "district2", "district3", "district4", "district5", "district6", "district7", "district8", "district9"];
+
+websocket.on("connection", (socket) => {
+    socket.on("joinChat", (districtId: string) => {
+        if (rooms.includes(districtId)) {
+            socket.join(districtId);
+        } else {
+            socket.emit("error", {message: "Invalid district ID"});
+        }
+    });
+
+    socket.on("message", ({districtId, message}) => {
+        if (rooms.includes(districtId)) {
+            websocket.to(districtId).emit("message", message);
+        }
+    });
+});
+
+websocket.listen(3000);
