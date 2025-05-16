a15a2a97d89e4dc5acae011ad10cf30ef3f7d9fa
peanthony <anthony.pereira@ynov.com>
2025-05-16 10:05:38 +0200

    rajouté des photo et théchnologie manque une photo

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -7,6 +7,7 @@ const devTeamMembers: TeamMember[] = [
     first_name: "Thomas",
     last_name: "Foltzer",
     class: "B3",
+    profile_picture_url: "/membersPicture/FoltzerThomas.jpg"
   },
   {
     first_name: "Alexis",
@@ -18,6 +19,7 @@ const devTeamMembers: TeamMember[] = [
     first_name: "Envel",
     last_name: "Righini",
     class: "B3",
+    profile_picture_url: "/membersPicture/RighiniEnvel.jpg" 
   },
   {
     first_name: "Anthony",
@@ -32,6 +34,7 @@ const infraCyberTeamMember: TeamMember[] = [
     first_name: "Mayeul",
     last_name: "Boeri",
     class: "B3",
+    profile_picture_url: "/membersPicture/BoeriMayeul.jpg"
   },
   {
     first_name: "Dorian",
@@ -42,11 +45,13 @@ const infraCyberTeamMember: TeamMember[] = [
     first_name: "Corentin",
     last_name: "Delpree",
     class: "B2",
+    profile_picture_url: "/membersPicture/DelpreeCorentin.jpg"
   },
   {
     first_name: "Mathieu",
     last_name: "Marchand",
     class: "B1",
+    profile_picture_url: "/membersPicture/MarchandMathieu.jpg"
   }
 ]
 
@@ -55,11 +60,13 @@ const dataTeamMember: TeamMember[] = [
     first_name: "Corentin",
     last_name: "Sanjuan",
     class: "B3",
+    profile_picture_url: "/membersPicture/SanjuanCorentin.png"
   },
   {
-    first_name: "Berriche",
-    last_name: "Yousri",
+    first_name: "Yousri",
+    last_name: "Berriche",
     class: "B1",
+    profile_picture_url: "/membersPicture/BerricheYousri.jpg"
   }
 ]
 
@@ -90,10 +97,45 @@ const pythonTech: UsedTech[] = [
   }
 ]
 
-const reactTech: UsedTech[] = [
+const devTech: UsedTech[] = [
   {
     name: "Next.js",
-    techLogo: "/technologies/react/nextLogo.png"
+    techLogo: "/technologies/dev/nextLogo.png"
+  },
+  {
+    name: "MQTT",
+    techLogo: "/technologies/dev/mqttLogo.png"
+  },
+  {
+    name: "tailwind",
+    techLogo: "/technologies/dev/tailwindLogo.png"
+  },
+  {
+    name: "websocket.io",
+    techLogo: "/technologies/dev/websocketioLogo.png"
+  },
+  {
+    name: "Postgressql",
+    techLogo: "/technologies/dev/postgresLogo.png"
+  }
+]
+
+const infraTech: UsedTech[] = [
+  {
+    name: "docker",
+    techLogo: "/technologies/infra/dockerLogo.png"
+  },
+  {
+    name: "Ubuntu",
+    techLogo: "/technologies/infra/ubuntuLogo.png"
+  },
+  {
+    name: "portainer",
+    techLogo: "/technologies/infra/portainerLogo.png"
+  },
+  {
+    name: "traefik",
+    techLogo: "/technologies/infra/traefikLogo.png"
   }
 ]
 
@@ -101,6 +143,7 @@ export default function Page() {
   const noIcon = "/membersPicture/noProfilePicture.png";
   return (
     <div className="flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl mt-15">SentiLyon</h1>
       <h1 className="font-bold text-4xl mt-15" >Presentation Du Projet</h1>
       <h2 className="mt-10 text-3xl mb-20" > Notre sujet</h2> 
       <div className="flex flex-row items-center gap-8 ml-[15%] mr-[15%]">
@@ -162,24 +205,30 @@ export default function Page() {
         {pythonTech.map((member, index) => (
           <div key={`data-${index}`} className="grid grid-cols-2 items-center gap-8 ml-[5%] mr-[5%] mb-5">
           <p className="text-xl">{member.name}</p>
-        <div className="w-32 h-32 rounded-full overflow-hidden ml-3">
+        <div className="w-32 h-32 overflow-hidden ml-3">
         <Image src={member.techLogo ?? noIcon} alt={`${member.name} logo`} width={200} height={200} />
         </div>
       </div>        
     ))}
       <h3 className="text-2xl mb-20 mt-30">Dev</h3>
-      <h4 className="text-xl mb-10">React</h4>
-      {reactTech.map((member, index) => (
+      {devTech.map((member, index) => (
         <div key={`data-${index}`} className="grid grid-cols-2 items-center gap-8 ml-[5%] mr-[5%] mb-5">
           <p className="text-xl">{member.name}</p>
-          <div className="w-32 h-32 rounded-full overflow-hidden ml-3">
+          <div className="w-32 h-32 overflow-hidden ml-10">
             <Image src={member.techLogo ?? noIcon} alt={`${member.name} logo`} width={200} height={200} />
           </div>
         </div>        
     ))}
 
     <h3 className="text-2xl mb-20 mt-30"> Infra/Cyber</h3>
-    <h4 className="text-xl mb-10">Docker</h4>
+    {infraTech.map((member, index) => (
+        <div key={`data-${index}`} className="grid grid-cols-2 items-center gap-8 ml-[5%] mr-[5%] mb-5">
+          <p className="text-xl">{member.name}</p>
+          <div className="w-32 h-32 overflow-hidden ml-10">
+            <Image src={member.techLogo ?? noIcon} alt={`${member.name} logo`} width={200} height={200} />
+          </div>
+        </div>        
+    ))}
     
     </div> 
   );
